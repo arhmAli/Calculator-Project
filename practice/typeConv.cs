ab95f8c730161d0a853ce74292aa80ddb205be52
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-20 14:23:13 +0500

    created ITC

---
+++ practice/typeConv.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace ITC{
+
+
+    public class ITC{
+   
+public static main void(){
+int x=5;
+
+long l=x;
+
+float f=x;
+
+
+}}}
+//name of class and namespace cannot be same
