6b2ffff0a36bb6b44047e100d8e28ce0e25df0c1
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-20 14:30:51 +0500

    created ETC

---
+++ practice/ETC.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace etcexample{
+
+
+public class ETC{
+
+
+public static void(){
+
+double i=1736187.17813;
+
+int j= (int)i
+
+console.writeLine("The value is explicitly converted from {0} to {1}",i,j)
+
+}
+
+}
+}
