4e70bf4c92c2907e3b57afa4e875b511251d10d3
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-20 14:35:23 +0500

    created boxing

---
+++ practice/boxing.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace boxing{
+
+
+public class boxinghere{
+
+public static void(){
+
+/boxed the value/ 
+
+object n=500;
+
+/Unboxed the value here/
+int i=n;
+
+System.console.writeLine("Value of n is {0}",n)
+}
+}
+}
