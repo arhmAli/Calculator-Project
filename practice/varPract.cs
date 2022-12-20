6d83660048f796e2d5f29b17e8891521e4af7c23
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-20 14:18:05 +0500

    createdVarPrac

---
+++ practice/varPract.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace integerBlock{
+
+public class area{
+
+public static void(){
+
+const double pi=3.14159;
+
+double r;
+
+r=10;
+
+double area=pi*r^2;
+
+double circum=2*pi*r;
+
+
+console.writeLine("The area of given circle is {0}",area);
+
+console.writeLine("The circum is {0}",circum)
+}
+
+}
+}
