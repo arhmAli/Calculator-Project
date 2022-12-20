4192d9bd65a03467c77c1dceb99945e583b0bdad
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-20 13:44:46 +0500

    created addition

---
+++ practice/Addition.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Addition{
+
+
+static public void Main(){
+
+int x=5;
+int y=7;
+int z;
+
+//************Addition Operation*****************
+
+z=x+y;
+
+
+console.writeLine("The sum is : {0}",z)
+}
+
+}
+
+
+//the {0} tells the cs that it is integer that we are trying to display
