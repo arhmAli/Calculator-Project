9ecd991a4a28db0a4f67e00c853b200ebfdd0599
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-20 15:17:45 +0500

    created ifSC

---
+++ practice/statement.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace IFSTATEMENT{
+
+class check{
+
+public static void Main(String[] args){
+
+
+int x=5;
+
+if (x>5){
+console.WriteLine("True")
+}
+else{
+console.WriteLine("false")
+}
+}
+}
+
+}
