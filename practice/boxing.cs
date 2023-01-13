6b7dceeeae2056aa0c011ca768c976dad6055510
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-13 19:03:31 +0500

    updated boxing logic

--- practice/boxing.cs
+++ practice/boxing.cs
@@ -7,11 +7,19 @@ public class boxinghere{
 
 public static void(){
 
-/boxed the value/ 
+//boxed the value
 
 object n=500;
-
-/Unboxed the value here/
+int j=10;
+int l=20;
+ if l>j{
+ System.Console.WriteLine("The value of L is greater than J)
+ }
+ else{
+ System.Console.WriteLine("The value of J is greater than L")
+ }
+             //We can also take the value from the user
+//Unboxed the value here
 int i=n;
 
 System.console.writeLine("Value of n is {0}",n)
